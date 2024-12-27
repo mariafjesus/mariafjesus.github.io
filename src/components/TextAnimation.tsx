6106e81dbadfe91990d45a69e54826e6e8822484
Maria Jesus <mariafjesus4@gmail.com>
2024-12-27 21:27:44 +0000

    Add animation to words

---
+++ src/components/TextAnimation.tsx
@@ -0,0 +1,16 @@
+import './../App.css'
+import './../css/TextAnimation.css'
+
+function TextAnimation() {
+    return (
+        <div className="glass-card word-container">
+            <h1 className="word">Maria Jesus</h1>
+            <h1 className="word">Resilient</h1>
+            <h1 className="word">Creative</h1>
+            <h1 className="word">Passionate</h1>
+            <h1 className="word">Hardworking</h1>            
+        </div>
+    )
+}
+
+export default TextAnimation
