97f3c5e018af359bdfec423f10616cda455841f1
Maria Jesus <mariafjesus4@gmail.com>
2024-12-27 21:35:02 +0000

    Update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/mariafjesus.github.io/', // Set this to your repository name
   plugins: [react()],
-})
+});
